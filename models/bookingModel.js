b1fcd85eff2cfff660ec2948ee4acb575a835810
kath193 <kattherinedecano@gmail.com>
2025-10-23 20:11:44 +0800

    Modify booking status options in bookingModel
    Updated booking status enum to include 'completed'.

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-guestId: { type: mongoose.Schema.Types.ObjectId, ref: 'Guest', required: true },
-roomId: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
-checkIn: { type: Date, required: true },
-checkOut: { type: Date, required: true },
-status: { type: String, enum: ['booked', 'checked-in', 'checked-out', 'cancelled'], default: 'booked', required: true }
-}, { timestamps: true });
+  guestId: { type: mongoose.Schema.Types.ObjectId, ref: 'Guest', required: true },
+  roomId: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
+  checkIn: { type: Date, required: true },
+  checkOut: { type: Date, required: true },
+  status: { type: String, enum: ['booked', 'checked-in', 'completed'], default: 'booked' }
+});
 
 module.exports = mongoose.model('Booking', bookingSchema);
 

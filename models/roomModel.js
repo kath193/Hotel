6f3f9a4bd377ec8252692a8177c50e86798bff4c
kath193 <kattherinedecano@gmail.com>
2025-10-23 20:13:29 +0800

    Refactor roomSchema definition in roomModel.js

--- models/roomModel.js
+++ models/roomModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
-number: { type: Number, required: true, unique: true },
-type: { type: String, required: true },
-price: { type: Number, required: true, min: 0 },
-status: { type: String, enum: ['available', 'occupied'], default: 'available', required: true }
-}, { timestamps: true });
+  number: { type: Number, required: true, unique: true },
+  type: { type: String, required: true },
+  price: { type: Number, required: true },
+  status: { type: String, enum: ['available', 'occupied'], default: 'available' }
+});
 
 module.exports = mongoose.model('Room', roomSchema);
 

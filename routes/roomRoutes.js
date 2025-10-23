1bdef57cf1c3db85a63265a77b18f30f12980eb3
kath193 <kattherinedecano@gmail.com>
2025-10-23 20:18:40 +0800

    Update roomRoutes.js

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const roomCtrl = require('../controllers/roomController');
+const { getAllRooms, createRoom, updateRoom, deleteRoom } = require('../controllers/roomsController');
 
-router.get('/rooms', roomCtrl.getRooms);
-router.get('/rooms/:id', roomCtrl.getRoom);
-router.post('/rooms', roomCtrl.createRoom);
-router.put('/rooms/:id', roomCtrl.updateRoom);
-router.delete('/rooms/:id', roomCtrl.deleteRoom);
+router.get('/', getAllRooms);
+router.post('/', createRoom);
+router.put('/:id', updateRoom);
+router.delete('/:id', deleteRoom);
 
 module.exports = router;
-
 

19f7e4ae8626b87d5e204f85df44f0a158452e16
kath193 <kattherinedecano@gmail.com>
2025-10-23 20:03:43 +0800

    Update roomController.js

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,51 +1,36 @@
-const Room = require('../models/roomModel');
+const Room = require('../models/Room');
 
-exports.getRooms = async (req, res) => {
-try {
-const rooms = await Room.find();
-res.json(rooms);
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
-};
-
-exports.getRoom = async (req, res) => {
-try {
-const room = await Room.findById(req.params.id);
-if (!room) return res.status(404).json({ error: 'Room not found' });
-res.json(room);
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
+exports.getAllRooms = async (req, res) => {
+  const rooms = await Room.find();
+  res.json(rooms);
 };
 
 exports.createRoom = async (req, res) => {
-try {
-const newRoom = await Room.create(req.body);
-res.status(201).json(newRoom);
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
+  try {
+    const newRoom = new Room(req.body);
+    await newRoom.save();
+    res.status(201).json(newRoom);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 exports.updateRoom = async (req, res) => {
-try {
-const updatedRoom = await Room.findByIdAndUpdate(req.params.id, req.body, { new: true });
-if (!updatedRoom) return res.status(404).json({ error: 'Room not found' });
-res.json(updatedRoom);
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
+  try {
+    const room = await Room.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(room);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 exports.deleteRoom = async (req, res) => {
-try {
-const deleted = await Room.findByIdAndDelete(req.params.id);
-if (!deleted) return res.status(404).json({ error: 'Room not found' });
-res.json({ message: 'Room deleted' });
-} catch (err) {
-res.status(500).json({ error: err.message });
-}
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Room deleted' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 

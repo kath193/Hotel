d033cdaafff44b52549a95a5e2835be2e84ae15a
kath193 <kattherinedecano@gmail.com>
2025-10-23 20:12:39 +0800

    Fix formatting in guestModel.js

--- models/guestModel.js
+++ models/guestModel.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const guestSchema = new mongoose.Schema({
-name: { type: String, required: true },
-email: { type: String, required: true, unique: true },
-phone: { type: String, required: true }
-}, { timestamps: true });
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true }
+});
 
 module.exports = mongoose.model('Guest', guestSchema);
-
 

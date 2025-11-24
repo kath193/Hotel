b185cb498c13d91afd1a45689eb07d349e8a7f46
kath193 <kattherinedecano@gmail.com>
2025-11-24 18:07:08 +0800

    Update bookingsController.js

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -1,5 +1,5 @@
-const Booking = require('./models/Booking');
-const Room = require('./models/Room');
+const Booking = require('/models/Booking');
+const Room = require('/models/Room');
 
 // CREATE
 exports.createBooking = async (req, res) => {

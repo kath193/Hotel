a600e25c0072630f6a428b18028418e09f887d12
kath193 <kattherinedecano@gmail.com>
2025-11-24 17:19:26 +0800

    Refactor booking routes and controller imports

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getAllBookings, createBooking, updateBooking, deleteBooking } = require('../controllers/bookingsController');
+const {
+  createBooking,
+  getBookings,
+  getBooking,
+  updateBooking,
+  deleteBooking
+} = require('../controllers/bookingController');
 
-router.get('/', getAllBookings);
 router.post('/', createBooking);
+router.get('/', getBookings);
+router.get('/:id', getBooking);
 router.put('/:id', updateBooking);
 router.delete('/:id', deleteBooking);
 
 module.exports = router;
-
 

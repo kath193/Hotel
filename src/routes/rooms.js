659bf7e8daf40906d877465ba9116e3ad7f195db
kath193 <kattherinedecano@gmail.com>
2025-11-24 19:06:45 +0800

    Update rooms.js

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -6,7 +6,7 @@ const {
   getRoom,
   updateRoom,
   deleteRoom
-} = require('./controllers/roomController');
+} = require('../controllers/roomController');
 
 router.post('/', createRoom);
 router.get('/', getRooms);
@@ -15,5 +15,6 @@ router.put('/:id', updateRoom);
 router.delete('/:id', deleteRoom);
 
 module.exports = router;
+
 
 
